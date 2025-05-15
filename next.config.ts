70e6413d4a946f24cf9d885640c6391a3fafcd98
Alexander Tolkachev <alexandr.tolkachyov@gmail.com>
2025-05-15 11:12:46 +0800

    added gh-pages package

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/alextolk",
+  assetPrefix: "/alextolk/",
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
 };
 
