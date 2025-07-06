502c274e87512a538262835f0c5a672c6149dece
Aleksandr Tolkachev <alexandr.tolkachyov@gmail.com>
2025-07-06 20:21:01 +0800

    Finalize config for GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  basePath: isProd ? '/alextolk' : '',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
